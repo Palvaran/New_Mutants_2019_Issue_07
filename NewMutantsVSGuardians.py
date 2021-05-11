bbc81494655163f21b7fdfffa265c8b3a528b76b
Palvaran <palvaran@hotmail.com>
2021-05-11 01:42:04 -0500

    Update NewMutantsVSGuardians.py

--- NewMutantsVSGuardians.py
+++ NewMutantsVSGuardians.py
@@ -3,8 +3,6 @@ mutants = ['Mirage', 'Smasher', 'Mondo', 'Chamber', 'Wolfsbane', 'Karma']
 mutant_survivors=len(mutants)
 guardians = ['Manta', 'Earthquake', 'Quasar', 'Starbolt', 'Flashfire', 'Hussar']
 guardian_survivors=len(guardians)
-die1 = random.randint(1,6)
-die2 = random.randint(1,6)
 print('ROUND ONE:')
 print('')
 print('Mutants:')
@@ -12,20 +10,29 @@ print(*mutants, sep =", ")
 print('Guardians:')
 print(*guardians, sep =", ")
 print('')
-mutant1 = (random.choice(mutants))
-print(mutant1 + ' rolled a ' + str(die1))
-guardian1 =(random.choice(guardians))
-print(guardian1 + ' rolled a ' + str(die2))
-if (die1 > die2):
-    print (mutant1 + ' wins! ' + (guardian1) + ' loses!')
-    guardians.remove(guardian1)
-elif (die1 < die2):
-    print (guardian1 + ' wins! ' + (mutant1) + ' loses!')
-    mutants.remove(mutant1)
-else:
-    print ('Its a double knockout!')
-    mutants.remove(mutant1)
-    guardians.remove(guardian1)
+count = 0
+while True:
+    print(count)
+    count += 1
+    if count >= 7:
+        break
+    mutant1 = (random.choice(mutants))
+    die1 = random.randint(1,6)
+    print(mutant1 + ' rolled a ' + str(die1))
+    guardian1 =(random.choice(guardians))
+    die2 = random.randint(1,6)
+    print(guardian1 + ' rolled a ' + str(die2))
+    if (die1 > die2):
+        print (mutant1 + ' wins! ' + (guardian1) + ' loses!')
+        guardians.remove(guardian1)
+    elif (die1 < die2):
+        print (guardian1 + ' wins! ' + (mutant1) + ' loses!')
+        mutants.remove(mutant1)
+    else:
+        print ('Its a double knockout!')
+        mutants.remove(mutant1)
+        guardians.remove(guardian1)
+    count += 1
 print('')
 mutant_survivors=len(mutants)
 print(str(mutant_survivors) + ' Mutants left!')
